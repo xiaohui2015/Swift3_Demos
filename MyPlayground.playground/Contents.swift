62f7a86d127bb0a542400626404b2e823e68fb61
fengxiaohui <18367107969@163.com>
2017-03-28 19:19:35 +0800

    Swift之“属性”

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -24,7 +24,7 @@ test1(speed: 100, time: 30) { (distance) -> Void in
 
 
 
-//但是可以用类型别名typealias的方式替换系统提供的类名，提高参数类型的可读性
+// 可以用类型别名typealias的方式替换系统提供的类名，提高参数类型的可读性
 typealias Distance = CGFloat
 func test2(speed: CGFloat, time: CGFloat, begin: (_ distance: Distance) -> Void) {
     
@@ -150,8 +150,139 @@ reducedArray
 
 
 
+// 图片资源通过枚举实现一一对应
+extension UIImage {
+    enum AssetIdentifier: String {
+        // Image Names of Minions
+        case Bob, Dave, Jorge, Jerry, Tim, Kevin, Mark, Phil, Stuart
+    }
+    
+    convenience init!(assetIdentifier: AssetIdentifier) {
+        self.init(named: assetIdentifier.rawValue)
+    }
+}
+let minionBobImage = UIImage(assetIdentifier: .Kevin)
 
 
 
+// 1.存储属性
+
+// a.结构体
+struct FixedLengthRange {
+    var firstValue:Int      // 变量存储属性
+    let length:Int          // 常量存储属性
+}
+let item1 = FixedLengthRange(firstValue: 10, length: 10)
+//item1.firstValue = 5 // 错误：不能修改常量结构体实例的存储属性
+var item2 = FixedLengthRange(firstValue: 10, length: 10)
+item2.firstValue = 5
 
 
+// b.类
+class Person {
+    var name: String = ""
+    let heart: Int = 1
+}
+let p1 = Person()
+p1.name = "xiaohui"
+p1.heart
+var p2 = Person()
+p2.name = "xiaohong"
+p2.heart
+
+
+
+// 2.计算属性
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+struct Rect {
+    
+    var origin = Point()
+    
+    var size = Size()
+    
+    var center: Point {          // 计算属性
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origin.x = newCenter.x - size.width / 2
+            origin.y = newCenter.y - size.height / 2
+        }
+//        set {            // 若不提供新值变量名，则默认为newValue
+//            origin.x = newValue.x - size.width / 2
+//            origin.y = newValue.y - size.height / 2
+//        }
+    }
+    
+    var maxX: Float {        // 只读属性，省略get{}
+        return Float(origin.x) + Float(size.width)
+    }
+}
+
+var rect = Rect(origin: Point(x: 0.0, y: 0.0), size: Size(width: 100, height: 100))
+rect.size.width
+rect.size.height
+rect.origin.x
+rect.origin.y
+rect.center.x
+rect.center.y
+rect.maxX
+
+let initialSquareCenter = rect.center
+rect.center = Point(x: 15.0, y:15.0)
+rect.center.x
+rect.center.y
+rect.origin.x
+rect.origin.y
+rect.maxX
+rect.size.width
+rect.size.height
+
+
+
+// 3.类型属性
+struct AudioChannel {
+    
+    static let threaholdLevel = 10
+    
+    static var maxInputLevelForAllChannels = 0
+    
+    var currentLevel: Int = 0 {
+        didSet{
+            if currentLevel > AudioChannel.threaholdLevel {
+                currentLevel = AudioChannel.threaholdLevel
+            }
+            
+            if currentLevel > AudioChannel.maxInputLevelForAllChannels {
+                AudioChannel.maxInputLevelForAllChannels = currentLevel
+            }
+        }
+    }
+}
+
+var leftChannel = AudioChannel()
+var rightChannel = AudioChannel()
+
+leftChannel.currentLevel = 7
+
+print(leftChannel.currentLevel)       // 7
+print(AudioChannel.maxInputLevelForAllChannels)   // 7
+
+rightChannel.currentLevel = 11
+print(rightChannel.currentLevel)      // 10
+print(AudioChannel.maxInputLevelForAllChannels)   // 10
+
+
+
+// 正则表达式
+var a = "^123."
+a
